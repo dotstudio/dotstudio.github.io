a31ac5c640299506cfa5be49a3c22bd41a498baa
n0bisuke <rsksound@gmail.com>
2016-05-13 01:46:17 +0900

    update gulpfile

---
+++ README.md
@@ -0,0 +1,7 @@
+
+準備
+
+```
+git clone git@github.com:dotstudio/dotstudio.github.io.git
+npm i
+```
--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,35 @@
 let fs = require('fs');
 let gulp = require("gulp");
 let ejs = require('gulp-ejs');
+let sass = require('gulp-sass');
 let gutil = require('gulp-util');
 let json = JSON.parse(fs.readFileSync("./package.json"));
 
 let paths = {
   templates: './src/templates/*.ejs',
   scripts: ['./src/js/*.ejs'],
-  images: 'client/img/**/*'
+  images: 'client/img/**/*',
+  styles: './src/scss/*.scss'
 };
 
+//EJS -> HTML
 gulp.task('templates', () => {
-
   return gulp.src(paths.templates)
-      .pipe(ejs(json,{'ext': '.html'}))
-      // .on('error', gutil.log))
-      // .pipe(ejs(json, ))
+      .pipe(ejs(json,{'ext': '.html'})
+      .on('error', gutil.log))
       .pipe(gulp.dest('./'));
+});
+
+//scss -> css
+gulp.task('styles', () => {
+  return gulp.src(paths.styles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css/'));
 });
 
 gulp.task('watch', () => {
   gulp.watch(paths.templates, ['templates']);
+  gulp.watch(paths.styles, ['styles']);
 });
 
-gulp.task('default', ['templates','watch']);
+gulp.task('default', ['templates','styles','watch']);
