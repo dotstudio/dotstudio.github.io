422c0582766116ce183c9cee64008d5690f21dd4
n0bisuke <rsksound@gmail.com>
2016-07-19 12:53:04 +0900

    deploy

--- js/app.js
+++ js/app.js
@@ -7,10 +7,16 @@
 function addClassSelecet(){
   var tmp = location.href.split('/');
   var url = tmp[0]+'//'+tmp[1]+tmp[2]+'/'+tmp[3]+'/';
+  var $current;
+
   //ブログページじゃなかったら
-  if (url.indexOf('/blog/') == -1) url = location.origin+'/';
-  var $current = $('.menu-item a[href="'+url+'"]')
-  $current.attr('class', 'sub_text_selected');
+  if (url.indexOf('/projects/') != -1 || url == location.href+'/' && url.indexOf('/404.html/') == -1){
+    $current = $('a[data-type="projects"]');
+    $current.attr('class', 'sub_text_selected');
+  }else if(url.indexOf('/blog/') != -1){
+    $current = $('a[data-type="blog"]');
+    $current.attr('class', 'sub_text_selected');
+  }
 }
 
 //ユーザーエージェントチェック
