bf0c3720814df7ac190e0d14e2fabdc481ab7781
のびすけ <rsksound@gmail.com>
2017-02-21 01:57:36 +0900

    deploy

--- js/app.js
+++ js/app.js
@@ -1,78 +1,55 @@
-// (function(){
-//   addClassSelecet();
-//   spHack();
-// })();
+'use strict'
 
-// //メニューのカレント状態切り替え
-// function addClassSelecet(){
-//   var tmp = location.href.split('/');
-//   var url = tmp[0]+'//'+tmp[1]+tmp[2]+'/'+tmp[3]+'/';
-//   var $current;
-//   var type = tmp[3];
-//   // console.log(url, tmp);
-//   if(url.indexOf('/legal/') != -1 || 
-//     url.indexOf('/about/') != -1 ||
-//     url.indexOf('/docs/') != -1 ||
-//     url.indexOf('/404.html/') != -1){
+// h2,h3までの目次divを作る
+function tableOfContents($domlist){
+    var $ol = document.createElement('ol');
+    var $liList = [];
 
-//   }
-  
-//   //ブログページ
-//   else if(type === '' || type === 'blog' || type === 'series'){
-//     $current = $('a[data-type="blog"]');
-//     $current.attr('class', 'sub_text_selected');
-//     //sp
-//     $current = $('p[data-type="sp-nuv-main"] a');
-//     $current.text('BLOG');
-//     $current.attr('href', '/');
-//     $current = $('p[data-type="sp-nuv-sub"] a');
-//     $current.text('SHOP');
-//     $current.attr('href', '/projects');
-//   }
+    for(var i = 0,len=$domlist.length; i < len; i++){
+        if($domlist[i].id === '')continue;
+        if($domlist[i].tagName === 'H2'){
+            /*H2目次を生成*/
+            //リンク生成
+            var $a = document.createElement('a');
+            $a.href = '#'+$domlist[i].id;
+            $a.innerHTML = $domlist[i].textContent;
+            //リスト生成
+            var $li = document.createElement('li');
+            $li.appendChild($a);
+            $ol.appendChild($li);
+        }else if($domlist[i].tagName === 'H3'){
+            //H3がある場合
+            var $_li = document.createElement('li');
+            $_li.textContent = $domlist[i].textContent;
+            if($domlist[i-1].tagName !== 'H3'){
+                var $ul = document.createElement('ul');
+                $ul.appendChild($_li);
+                $ol.lastElementChild.appendChild($ul);
+            }else{
+                //h3が続く場合
+                $ul.appendChild($_li);
+            }
+        }
+    }
+    return $ol;
+}
 
-//   //プロジェクトページ
-//   if (type === 'projects'){
-//     $current = $('a[data-type="projects"]');
-//     $current.attr('class', 'sub_text_selected');
-//     //sp
-//     $current = $('p[data-type="sp-nuv-main"] a');
-//     $current.text('SHOP');
-//     $current.attr('href', '/projects');
-//     $current = $('p[data-type="sp-nuv-sub"] a');
-//     $current.text('BLOG');
-//     $current.attr('href', '/');
-//   }
+//blogページで目次をつける
+function appendToc(){
+    var $elem = document.querySelectorAll('.blog-single--content > h2,h3');
+    if($elem.length === 0)return;
+    console.log($elem);
+    var $toc = tableOfContents($elem);
+    var $firstHeader = document.querySelector('.blog-single--content h2');
+    var $div = document.createElement('div');
+    $div.className = 'blog-single--toc';
+    $div.id = 'mokuji';
+    $div.appendChild($toc);
 
-//   //About
-//   if (type === 'about'){
-//     $current = $('a[data-type="about"]');
-//     $current.attr('class', 'sub_text_selected');
-//   }
+    $firstHeader.parentNode.insertBefore($div,$firstHeader);
+}
 
-//   //docs
-//   if (type === 'docs'){
-//     $current = $('a[data-type="docs"]');
-//     $current.attr('class', 'sub_text_selected');
-//   }
+(function(){
+    appendToc();
+})();
 
-// }
-
-// //ユーザーエージェントチェック
-// function _uaCheck(){
-//   var ua = 'pc';
-//   if ((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1) || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0) {
-//     ua = 'sp';
-//   }
-//   return ua;
-// }
-
-// //だーてぃーはっく
-// function spHack(){
-//   var ua = _uaCheck();
-//   if(ua !== 'sp')return;
-
-//   $('.projectsContent-title').css({'width':'100%','font-size':'20px'});
-//   $('.row3').css('width','100%');
-//   $('.projectsContent-txt').css({'width':'100%','font-size':'14px'});
-//   $('.projectsContent-headImg').css('width','100%');
-// }
